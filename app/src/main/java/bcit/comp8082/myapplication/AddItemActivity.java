34511990efe61291ffdb178421cd5e1c968db34f
Haojun Liao <junliao@Haojuns-MacBook-Pro.local>
2022-02-07 02:21:45 -0800

    added save item list activity

---
+++ app/src/main/java/bcit/comp8082/myapplication/AddItemActivity.java
@@ -0,0 +1,38 @@
+package bcit.comp8082.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddItemActivity extends AppCompatActivity {
+    Button confirm;
+    Button cancel;
+    EditText itemName;
+    EditText price;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_item);
+        confirm = findViewById(R.id.item_confirm_btn);
+        cancel = findViewById(R.id.item_cancel_btn);
+        itemName = findViewById(R.id.item_name_input);
+        price = findViewById(R.id.item_price_input);
+    }
+
+    public void cancel(final View v){
+        finish();
+    }
+
+    public void confirm(final View v) {
+        Intent intent = new Intent();
+        intent.putExtra("ITEMPRICE", Double.parseDouble(price.getText().toString()));
+        intent.putExtra("ITEMNAME", itemName.getText().toString());
+        setResult(RESULT_OK, intent);
+        finish();
+    }
+}
---
+++ app/src/main/java/bcit/comp8082/myapplication/models/Item.java
@@ -0,0 +1,47 @@
+package bcit.comp8082.myapplication.models;
+
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
+
+public class Item {
+    private String name;
+    private double price;
+    private Bitmap image;
+    private String lastPurchase;
+
+    public Item(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Bitmap getImage() {
+        return image;
+    }
+
+    public void setImage(Bitmap image) {
+        this.image = image;
+    }
+
+    public String getLastPurchase() {
+        return lastPurchase;
+    }
+
+    public void setLastPurchase(String lastPurchase) {
+        this.lastPurchase = lastPurchase;
+    }
+}
